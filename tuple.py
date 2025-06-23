4c03e2d32c5a9244f8da23c70b2a2558ae66b89b
Prajwal Nikhar <prajwalnikhar@Prajwals-MacBook-Air.local>
2025-06-24 01:01:24 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,38 @@
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+print(type(thistuple))
+
+# NOT a tuple: To create a tuple with only one item, you have to add a 
+# comma after the item otherwise python will not recognize it as a 
+# tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+# Access tuple items
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[1])
+# negative indexing
+print(thistuple[-1])
+print(thistuple[-4:-1])
+# Range of indexes
+print(thistuple[2:5])
+print(thistuple[:4])
+print(thistuple[2:])
+
+# update tuple
+y = list(thistuple)
+y[1] = "kiwi"
+thistuple = tuple(y)
+print(thistuple)
+
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)
+
+print(y)
+y.remove("apple")
+thistuple = tuple(y)
+print(thistuple)
+
+del thistuple
+print(thistuple)
