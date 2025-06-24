7bbf2192bdaca0d11c41bd022b70e613e322451c
Prajwal Nikhar <prajwalnikhar@Prajwals-MacBook-Air.local>
2025-06-24 23:57:30 +0530

    set

---
+++ set.py
@@ -0,0 +1,7 @@
+myset = {"apple", "banana", "cherry", "apple"} # set cannot have two items with the same value
+print(myset)
+
+thisset = {"apple", "banana", "cherry", True, 1, 2} #True and 1 is considered as the same value
+print(thisset)
+
+print(len(thisset))
